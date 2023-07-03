4457be20388939817a31981c0bc328d32c13bafa
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-07-04 08:58:49 +1000

    Updated build script to make Universal app

--- Source/build.sh
+++ Source/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller --onefile --add-binary='/opt/homebrew/lib/libmediainfo.dylib:.' rename-avid-mxf.py
+pyinstaller --target-arch universal2 --onefile --add-binary='/opt/homebrew/lib/libmediainfo.dylib:.' rename-avid-mxf.py
